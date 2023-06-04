5ad022ec2c890538067b54efa3d80169215e1f6c
f957-cua <ivanfiliptschik00@gmail.com>
2023-06-04 23:28:40 +0300

    upload on web

---
+++ scripts/app.js
@@ -0,0 +1,241 @@
+'use strict';
+
+let habbits = [
+    {
+        id: 1,
+        icon: "work_out",
+        name: " push-ups",
+        target: 10,
+        days: [
+            {comment: "The first set is always the hardest"},
+            {comment: "The second is much easier"}
+        ]
+    },
+    {
+        id: 2,
+        icon: "food",
+        name: "proper nutrition",
+        target: 10,
+        days: [{comment: "Good things"}]
+    }
+];
+const HABBIT_KEY = "HABBIT_KEY";
+let globalActiveHabbitId;
+
+/* page */
+
+const page = {
+    menu: document.querySelector(".menu__list"),
+    header: {
+        h1: document.querySelector(".h1"),
+        progressPercent: document.querySelector(".progress__percent"),
+        progressCoverBar: document.querySelector(".progress__cover-bar"),
+    },
+    content: {
+        daysContainer: document.getElementById("days"),
+        nextDay: document.querySelector(".habbit__day")
+    },
+    popup: {
+        index: document.getElementById("add-habbit-popup"),
+        iconField: document.querySelector(".popup__form input[name='icon']"),
+    }
+}
+
+/* utils */
+
+function loadData() {
+    const habbitsString = localStorage.getItem(HABBIT_KEY);
+    const habbitArray = JSON.parse(habbitsString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
+    }
+}
+
+function saveData() {
+    const habbitsString = JSON.stringify(habbits);
+    localStorage.setItem(HABBIT_KEY, habbitsString);
+}
+
+
+function togglePopup() {
+    if (page.popup.index.classList.contains("cover__hidden")) {
+        page.popup.index.classList.remove("cover__hidden")
+    } else {
+        page.popup.index.classList.add("cover__hidden")
+    }
+}
+
+function resetForm(form, fields) {
+    for (const field of fields) {
+        form[field].value = "";
+    }
+}
+
+function validateAndGetFormData(form, fields) {
+    const formData = new FormData(form);
+    const res = {};
+    for (const field of fields) {
+        const fieldValue = formData.get(field);
+        form[field].classList.remove("error");
+        
+        if (!fieldValue) {
+            form[field].classList.add("error");
+        }
+        res[field] = fieldValue;
+    }
+    let isValid = true;
+    for (const field of fields) {
+        if (!res[field]) {
+            isValid = false;
+        }
+    }
+    if (!isValid) {
+        return;
+    }
+    return res;
+}
+
+/* render */
+
+function rerenderMenu(activeHabbit) {
+    for (let habbit of habbits) {
+        const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+        if (!existed) {
+            const element = document.createElement("button");
+            element.setAttribute("menu-habbit-id", habbit.id);
+            element.classList.add("menu__item");
+            element.addEventListener("click", () => rerender(habbit.id));
+            element.innerHTML = `<img src="/images/${habbit.icon}.svg" alt="${habbit.name}" />`
+            if (activeHabbit.id === habbit.id) {
+                element.classList.add("menu__item_active");
+            };
+            page.menu.appendChild(element);
+            continue;
+        }
+        if (activeHabbit.id === habbit.id) {
+            existed.classList.add("menu__item_active");
+        } else {
+            existed.classList.remove("menu__item_active");
+        }
+
+    }
+}
+
+function renderHead(activeHabbit) {
+    page.header.h1.innerText = activeHabbit.name;
+    const progress = activeHabbit.days.length / activeHabbit.target > 1
+        ? 100
+        : activeHabbit.days.length / activeHabbit.target * 100;
+    page.header.progressPercent.innerText = progress.toFixed(0) + "%";
+    page.header.progressCoverBar.setAttribute("style", `width: ${progress}%`);
+}
+
+function renderContent(activeHabbit) {
+    page.content.daysContainer.innerHTML = "";
+    for (const index in activeHabbit.days) {
+        const element = document.createElement("div");
+        element.classList.add("habbit");
+        element.innerHTML = `<div class="habbit__day">Day ${Number(index) + 1}</div>
+                        <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
+                        <button class="habbit__delete" onclick="deleteDay(${index})">
+                            <img src="./images/delete.svg" alt="delete">
+                        </button>`
+        page.content.daysContainer.appendChild(element);
+    }
+    page.content.nextDay.innerHTML = `Day ${activeHabbit.days.length + 1}`;
+}
+
+
+/* work with days */
+
+function addDays(event) {
+    event.preventDefault();
+    const data = validateAndGetFormData(event.target, ["comment"]);
+    if (!data) {
+        return;
+    }
+    habbits = habbits.map(habbit => {
+        if (habbit.id === globalActiveHabbitId) {
+            return {
+                ...habbit,
+                days: habbit.days.concat([{comment: data.comment}])
+            }
+        }
+        return habbit;
+    })
+    resetForm(event.target, ["comment"])
+    rerender(globalActiveHabbitId);
+    saveData();
+}
+
+/* working with habbits */
+
+function setIcon(context, icon) {
+    page.popup.iconField.value = icon;
+    const activeIcon = document.querySelector(".icon.icon_active");
+    activeIcon.classList.remove("icon_active");
+    context.classList.add("icon_active");
+}
+
+function addHabbit(event) {
+    event.preventDefault();
+    const data = validateAndGetFormData(event.target, ["name", "icon", "target"]);
+    if (!data) {
+        return;
+    }
+    const maxId = habbits.reduce((acc, habbit) => acc > habbit.id ? acc : habbit.id, 0)
+    habbits.push({
+        id: maxId + 1,
+        name: data.name,
+        target: data.target,
+        icon: data.icon,
+        days: [],
+    })
+    resetForm(event.target, ["name", "target"]);
+    togglePopup();
+    saveData();
+    rerender(maxId + 1);
+}
+
+/* remove habbit */
+
+function deleteDay(index) {
+    habbits = habbits.map(habbit => {
+        if (habbit.id === globalActiveHabbitId) {
+            habbit.days.splice(index, 1);
+            return {
+                ...habbit,
+                days: habbit.days,
+            }
+        }
+        return habbit;
+    });
+    rerender(globalActiveHabbitId);
+    saveData();
+}
+
+
+function rerender(activeHabbitId) {
+    globalActiveHabbitId = activeHabbitId;
+    const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+    if (!activeHabbit) {
+        return;
+    };
+    document.location.replace(document.location.pathname + "#" + activeHabbitId);
+    rerenderMenu(activeHabbit);
+    renderHead(activeHabbit);
+    renderContent(activeHabbit);
+}
+
+/* init */
+
+(() => {
+    loadData();
+    const hashId = Number(document.location.hash.replace("#", ""));
+    const urlHabbit = habbits.find(habbit => habbit.id === hashId);
+    if (urlHabbit) {
+        rerender(urlHabbit.id)
+    } else {
+        rerender(habbits[0].id);
+    }
+})();
